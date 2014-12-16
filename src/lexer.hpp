7bbc71fd8e898d53dffe62ba1f2a165de8d4f9aa
Demetri Obenour <demetriobenour@gmail.com>
2014-12-15 21:53:57 -0500

    Header file included by both source files
    This header file is included by both source files, and holds function prototypes and a typedef.

---
+++ src/lexer.hpp
@@ -0,0 +1,17 @@
+#if defined(__cplusplus) &&__cplusplus >= 201103L
+# define NORETURN [[noreturn]]
+#elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+# define NORETURN _Noreturn
+#elif defined(__GNUC__) && __GNUC__ > 2 ||      \
+  (__GNUC__ == 2 && __GNUC_MINOR__ >= 5)
+# define NORETURN __attribute__((noreturn))
+#elif !defined(NORETURN)
+# define NORETURN
+#endif
+#include <vector>
+#include <string>
+NORETURN bool error(const unsigned char *msg);
+typedef std::vector<std::string> stringVector;
+stringVector lexer (unsigned char *input, int argc, char ** argv);
+
+NORETURN void die (const char *arg);
