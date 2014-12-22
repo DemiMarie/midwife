8840cb6f0aa3ac0fe8394cbd6a148009d0449f78
Demetri Obenour <demetriobenour@gmail.com>
2014-12-22 05:34:42 -0500

    Cleaned up autogen.sh to run right tasks in right order

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 var=${0%/*}
 cd -- "${var:-.}"
-autoreconf
-autom4te --language=autotest --output=tests/testsuite tests/testsuite.at
+
+# Clean cache
+rm autom4te.cache -r
+
+# M4 code in configure.ac package.m4 automatically
+rm package.m4
+
+autoreconf --install -Wall
+autom4te -Wall \
+	--language=autotest \
+	--output=tests/testsuite \
+	tests/testsuite.at
