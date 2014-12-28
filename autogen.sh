1381ca051d3e493e87c383e2c648dac798d0e7b9
Demetri Obenour <demetriobenour@gmail.com>
2014-12-28 21:32:03 +0200

    Added -Werror and changed rm -r to rm -rf

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 var=${0%/*}
 cd -- "${var:-.}"
 
 # Clean cache
-rm autom4te.cache -r
+rm autom4te.cache -rf
 
-# M4 code in configure.ac package.m4 automatically
-rm package.m4
+# M4 code in configure.ac generates package.m4 automatically
+rm -f package.m4 aclocal.m4
 
-autoreconf --install -Wall
-autom4te -Wall \
+autoreconf --install -Wall -Werror
+exec autom4te -Wall -Werror \
 	--language=autotest \
 	--output=tests/testsuite \
 	tests/testsuite.at
