b96bc7a86e164e6c5dbe045c38eb49810ee1d6f9
Demetri Obenour <demetriobenour@gmail.com>
2014-12-30 20:55:57 +0200

    git-build.sh: use -e option

--- git-build.sh
+++ git-build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh --
+set -e
 dir=${0%/*}
-cd -- "${dir:-.}" &&
-./autogen.sh &&
+cd -- "${dir:-.}"
+./autogen.sh
+mkdir build 2>/dev/null || {
 chmod 700 -R build &&
-rm -r build &&
-mkdir build &&
-cd build &&
-../configure "$@" &&
-make ${1+"-j$1"} &&
-make check &&
+rm -rf build &&
+mkdir build
+}
+(cd build
+../configure "$@"
+make ${1+"-j$1"}
+make check
 make distcheck
+make clobber)
+rmdir build/src
+rmdir build
